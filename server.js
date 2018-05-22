c1762d1cf7aa32d484faff8d417730814db5e434
Stephanie Brooks <sbrooks84591@hotmail.com>
2018-05-21 23:02:21 -0500

    reconfig server

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
 const express = require('express');
-const db = require('/data.db');
+const helmet = require('helmet');
+
+const userRouter = require('./userRoutes');
+const { logger, errorHandler } = require('./middleware');
 
 const server = express();
 
 server.listen(5000, () => {
-    console.log('=== The App is running on port 5000 ===')
+    console.log('===APP running on port 5000===')
 })
 
-server.get('/', (request, response) => {
-    response.send('<h1>GET REQUEST RECEIVED</h1>')
-})
+server.use(helmet());
+server.use(express.json());
+server.use(logger());
 
-server.get('/api/users', (request, response) => {
-    db.find()
-    .then( users => {
-        response.status(200).json(({users}))
-    })
+server.use('./api.users', userRouter);
 
-    .catch( err => {
-        response.status(500).json({error: 'SORRY CANNOT FIND THE DATA'});
-    })
-});
-
-server.get('/api/users/:id', (request, response) => {
-    const userId = request.params.params.id;
-
-    db.findById(usedId)
-    .then( user => {
-        response.json({user});
-    })
-    .catch(err => {
-        response.status(500).json({ error: 'WHOOPSIE! NO DATA FOR YOU'});
-    } )
-})
+server.use(errorHandler);
